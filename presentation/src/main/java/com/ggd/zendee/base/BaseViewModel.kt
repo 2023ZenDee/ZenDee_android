119280a361863cff7d3c52b7881a387b7fddcf09
PARAOOO <nakim3159@gmail.com>
2023-04-05 20:01:43 +0900

    upload

---
+++ presentation/src/main/java/com/ggd/zendee/base/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.ggd.zendee.base
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel() : ViewModel() {
+}
---
+++ presentation/src/main/java/com/ggd/zendee/feature/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ggd.zendee.feature.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ggd.zendee.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
