555c7fdba04bfb863af9a8726e04f3ceadc1fbcf
stev3j <mygajeong@gmail.com>
2023-04-06 21:27:26 +0900

    ✨ :: Add Splash Screen
    근데 아직 되는지 모름. 실행이 안됨;;

---
+++ presentation/src/main/java/com/ggd/zendee/feature/main/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.ggd.zendee.feature.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    var isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(3000)
+            _isLoading.value = false
+        }
+    }
+}
