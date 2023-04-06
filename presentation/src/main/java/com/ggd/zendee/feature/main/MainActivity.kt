1013436f8c55b1737686d9300c50125e0de9534c
stev3j <mygajeong@gmail.com>
2023-04-06 21:27:26 +0900

    ✨ :: ADD SPLASH SCREEN
    근데 아직 되는지 모름. 실행이 안됨;;

--- presentation/src/main/java/com/ggd/zendee/feature/main/MainActivity.kt
+++ presentation/src/main/java/com/ggd/zendee/feature/main/MainActivity.kt
@@ -2,11 +2,22 @@ package com.ggd.zendee.feature.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ggd.zendee.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        installSplashScreen().apply {
+            setKeepVisibleCondition {
+                viewModel.isLoading.value
+            }
+        }
+        supportActionBar?.hide()
     }
 }
