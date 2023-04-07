e2a93c6470075af0940e0b9f3d78ea607a5bc209
stev3j <mygajeong@gmail.com>
2023-04-07 21:14:37 +0900

    ✨ :: ADD START SCREEN

--- presentation/src/main/java/com/ggd/zendee/feature/main/MainActivity.kt
+++ presentation/src/main/java/com/ggd/zendee/feature/main/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.ggd.zendee.feature.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.ggd.zendee.R
+import com.ggd.zendee.feature.signup.SignupFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         installSplashScreen().apply {
             setKeepVisibleCondition {
                 viewModel.isLoading.value
             }
         }
-        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+
+        //TODO : 만약 처음 젠디를 실행하는 거라면 SignupScreen으로, 아니면 바로 MapScreen으로
+
+        val num: Int = 1
+
+        if (num == 1) {
+            val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.fragment, SignupFragment())
+            fragmentTransaction.commit()
+        } else {
+
+        }
+
+
+
     }
 }
